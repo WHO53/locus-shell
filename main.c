03f7ccd052be91459743ffb38262e41b3c469cbb
WHO53 <notwho53@gmail.com>
2024-10-27 23:29:10 +0530

    add qs panel

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <locus.h>
 #include "bar/bar.h"
 #include "launcher/launcher.h"
+#include "qs/qs.h"
 #include <glib.h>
 #include <stdio.h>
 
-Locus *instance[] = {&bar, &launcher};
+Locus *instance[] = {&bar, &launcher, &qs};
 
 int main(int argc, char *argv[]){
     locus_init(&bar, 100, 2);
@@ -19,7 +20,11 @@ int main(int argc, char *argv[]){
     locus_set_touch_callback(&launcher, touch_launcher);
     launcher_init();
 
-    locus_run_multi(instance, 2);
+    locus_init(&qs, 70, 40);
+    locus_create_layer_surface(&qs, "locus-sidebar", ZWLR_LAYER_SHELL_V1_LAYER_TOP, 0, 0);
+    locus_set_draw_callback(&qs, draw_qs);
+
+    locus_run_multi(instance, 3);
     g_thread_join(glib_thread);
     launcher_free_apps();
     locus_cleanup(&bar);
---
+++ qs/qs.h
@@ -0,0 +1,11 @@
+#ifndef QS_H
+#define QS_H
+
+#include <locus.h>
+
+extern Locus qs;
+
+void draw_qs(cairo_t *cr, int width, int height);
+
+#endif // QS_H
+
